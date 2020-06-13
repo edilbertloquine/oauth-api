40dac4625260f95119298f321b930811c26972b6
Edilbert Loquine <edilbert@whitewidget.com>
2020-06-13 13:28:35 +0800

    feat(domain): setup access token domain and tests

---
+++ src/domain/access_token/access_token.go
@@ -0,0 +1,29 @@
+package access_token
+
+import (
+	"time"
+)
+
+const (
+	expirationTime = 24
+)
+
+// AccessToken -
+type AccessToken struct {
+	AccessToken string `json:"access_token"`
+	UserID      int64  `json:"user_id"`
+	ClientID    int64  `json:"client_id"`
+	Expires     int64  `json:"expires"`
+}
+
+// GetNewAccessToken -
+func GetNewAccessToken() AccessToken {
+	return AccessToken{
+		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
+	}
+}
+
+// IsExpired -
+func (at AccessToken) IsExpired() bool {
+	return time.Unix(at.Expires, 0).Before(time.Now().UTC())
+}
